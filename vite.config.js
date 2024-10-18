8c185efd366514c3ff37d69307fd80cecba4d0ca
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-10-18 14:39:51 +0530

    github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "./",
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  build: { outDir: '..' },
+  // needed for github pages just put the repo name here
+  base: '/sonic-runner/', 
 });
